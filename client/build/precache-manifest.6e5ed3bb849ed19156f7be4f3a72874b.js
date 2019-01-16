d10bd92ec1a4499d578d418b6c58cf2176eb1afa
LJ O <ljoks0727@gmail.com>
2019-01-16 19:13:40 +0000

    initial commit

---
+++ client/build/precache-manifest.6e5ed3bb849ed19156f7be4f3a72874b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa80f2744c90a84c6ade",
+    "url": "/static/js/main.fa80f274.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "3d130f416ecfa3967c08bc6227305ac8",
+    "url": "/index.html"
+  }
+];
