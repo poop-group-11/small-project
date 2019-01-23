0e997832de0b2caeb255bb3465b92d4071bde49c
NoahDelGrosso <46506203+NoahDelGrosso@users.noreply.github.com>
2019-01-23 15:34:03 -0500

    UserSchema and ContactSchema

---
+++ server/user_account.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema and Model
+const UserSchema = new Schema(
+  {
+    id: Number,
+    username: String,
+    pass: String,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+// Export so we can use in other files in this project
+module.exports = User;
