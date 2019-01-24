1f1b2d0c4874b612018f3a29ffa3b922228c3e66
NoahDelGrosso <46506203+NoahDelGrosso@users.noreply.github.com>
2019-01-24 17:39:34 -0500

    contact_info.js with edits

---
+++ server/contact_info.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema and Model
+const NumberSchema = new Schema(
+  {
+    home: Number,
+    cell: Number,
+    business: Number
+  }
+);
+
+const ContactSchema = new Schema(
+  {
+    id: Number,
+    user: Number,
+    fname: String,
+    lname: String,
+    email: String,
+    numbers: [NumberSchema],
+    address: String
+  },
+  { timestamps: true }
+);
+
+const Contact = mongoose.model('Contact', ContactSchema);
+
+// Export so we can use in other files in this project
+module.exports = Contact;
