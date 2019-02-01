c63da237bba53d6afcf2851a2b8d06649f07358c
LJ O <ljoks0727@gmail.com>
2019-02-01 16:50:53 +0000

    remove phone number schema to make our lives easy

--- server/contact_info.js
+++ server/contact_info.js
@@ -2,23 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
-// Create Schema and Model
-const NumberSchema = new Schema(
-  {
-    home: Number,
-    cell: Number,
-    business: Number
-  }
-);
 
 const ContactSchema = new Schema(
   {
     user_id: {type:ObjectId, ref: 'User'},
-    user: Number,
     fname: String,
     lname: String,
     email: String,
-    numbers: [NumberSchema],
+    numbers: Number,
     address: String
   },
   { timestamps: true }
